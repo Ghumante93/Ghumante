2120182be645a21f05ca087c593cfb0166957930
Ghumante93 <ghumante93@gmail.com>
2026-06-08 01:50:35 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,7 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+// GHUMANTE Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyC46M1m7hz9Jwh3jfTzv8OQpSdBXXyaWT4",
   authDomain: "ghumante-761f6.firebaseapp.com",
@@ -9,10 +6,9 @@ const firebaseConfig = {
   projectId: "ghumante-761f6",
   storageBucket: "ghumante-761f6.firebasestorage.app",
   messagingSenderId: "1042573497626",
-  appId: "1:1042573497626:web:6afea56a2c9fd2adc488a3"
+  appId: "1:1042573497626:web:29edd7ea6320736fc488a3"
 };
 
-const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app);
-export const auth = getAuth(app);
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
